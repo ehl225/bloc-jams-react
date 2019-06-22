4e9507f4671358ec7613b0c12cded1a9820f174d
Erica <ehlam225@gmail.com>
2019-06-22 15:51:19 -0400

    Added play/pause icons for currently played song

--- src/components/Album.js
+++ src/components/Album.js
@@ -36,25 +36,26 @@ class Album extends Component {
 }
 	showIcon(song, i) {
 		const isSameSong= this.state.currentSong === song;
-			if((this.state.hover===song && !isSameSong) || 
-(this.state.hover===song && isSameSong && !this.state.isPlaying)) {
-		return <span className='icon ion-md-play'></span>
+		if((this.state.hover===song && !isSameSong) || 
+(this.state.hover===song && isSameSong && !this.state.isPlaying 
+|| !this.state.isPlaying && isSameSong ))
+		{ return <span className='icon ion-md-play'></span>
 		} else if ((this.state.hover===song && this.state.isPlaying 
-&& isSameSong))
-			{
-			return <span className = 'icon ion-md-pause'></span>
-		} else {
-			return i+1;
+&& isSameSong || this.state.isPlaying && isSameSong))
+		{ return <span className = 'icon ion-md-pause'></span>
+		}
+		else  {
+		return i + 1;
 }
 }
 	handleSongClick(song) {
 		const isSameSong = this.state.currentSong === song;
 		if (this.state.isPlaying && isSameSong) {
 			this.pause();
-			} else {
-				if (!isSameSong) {this.setSong(song); }
-				this.play();
-			}
+		} else {
+			if (!isSameSong) {this.setSong(song);}
+			this.play();
+}
 }
 	render() {
 	return (
