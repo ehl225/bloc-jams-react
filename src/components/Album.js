4720653a1e3090335f6785cacf7b75b3c4592dd7
Erica <ehlam225@gmail.com>
2019-06-24 13:07:38 -0400

    Added current time and duration functionality to playerbar

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,12 +12,36 @@ class Album extends Component {
 		album: album,
 		currentSong: album.songs[0],
 		isPlaying: false,
-		hover: false
+		hover: false,
+		currentTime: 0,
+		duration: album.songs[0].duration
 		};
 	this.audioElement = document.createElement('audio');
 	this.audioElement.src = album.songs[0].audioSrc;
 }
-	play() {
+	componentDidMount() {
+		this.eventListeners = {
+			timeupdate: e => {
+				this.setState({ currentTime: 
+this.audioElement.currentTime});
+},
+			durationchange: e => {
+				this.setState({ duration: 
+this.audioElement.duration});
+}
+};			this.audioElement.addEventListener('timeupdate', 
+this.eventListeners.timeupdate);
+			this.audioElement.addEventListener('durationchange', 
+this.eventListeners.durationchange); 
+}
+	componentWillUnmount() {
+		this.audioElement.src = null;
+		this.audioElement.removeEventListener('timeupdate', 
+this.eventListeners.timeupdate);
+		this.audioElement.removeEventListener('durationchange', 
+this.eventListeners.durationchange);
+}
+play() {
 		this.audioElement.play();
 		this.setState({isPlaying: true});
 }
@@ -71,7 +95,12 @@ this.state.currentSong===song);
 		} else {
 			if (!isSameSong) {this.setSong(song);}
 			this.play();
-			}
+		}
+	}
+	handleTimeChange(e) {
+		const newTime = this.audioElement.duration * e.target.value;
+		this.audioElement.currentTime = newTime;
+		this.setState({currentTime: newTime});
 }
 	render() {
 	return (
@@ -120,6 +149,9 @@ index)}
 		handleSongClick={() =>this.handleSongClick(this.state.currentSong)}
 		handlePrevClick={() => this.handlePrevClick()}
 		handleNextClick={() => this.handleNextClick()}
+		currentTime={this.audioElement.currentTime}
+		duration={this.audioElement.duration}
+		handleTimeChange={(e) => this.handleTimeChange(e)}
 		/>
 	</section>
 	);
